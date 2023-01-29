60bafb18f04921502eb2285721a1fca875abd209
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-01-29 00:32:04 -0600

    move pokemon_data after status_code check

--- poke-script/pokescript.sh
+++ poke-script/pokescript.sh
@@ -15,9 +15,9 @@ else
   request="${POKEAPI_BASE_URL}${args}"
   response=$(curl -s -w '%{http_code}' "${request}")
   status_code=${response:(-3)}
-  pokemon_data=${response::-3}
 
   if [[ $status_code -eq 200 ]]; then
+    pokemon_data=${response::-3}
     data=$(echo "${pokemon_data}" | jq -r '"Id=\(.id), name=\(.name), weight=\(.weight), height=\(.height), order=\(.order)"')
     echo "${data}"
   else
