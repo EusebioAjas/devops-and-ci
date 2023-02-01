5b67d1ee6ece181aa58a80664aa71e36cb9dc747
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-01-31 20:18:35 -0600

    refactor: replace nested conditional

--- poke-script/pokescript.sh
+++ poke-script/pokescript.sh
@@ -11,18 +11,18 @@ fi
 if [[ $# -ne 1 ]]; then
   echo "Incorrect number of arguments passed. Only one argument is allowed."
   exit 1
-else
-  request="${POKEAPI_BASE_URL}${args}"
-  response=$(curl -s -w '%{http_code}' "${request}")
-  status_code=${response:(-3)}
+fi
 
-  if [[ $status_code -eq 200 ]]; then
-    pokemon_data=${response::-3}
-    data=$(echo "${pokemon_data}" | jq -r '"Id=\(.id), name=\(.name), weight=\(.weight), height=\(.height), order=\(.order)"')
-    echo "${data}"
-  else
-    echo "Pokemon not found."
-    exit 1
-  fi
+request="${POKEAPI_BASE_URL}${args}"
+response=$(curl -s -w '%{http_code}' "${request}")
+status_code=${response:(-3)}
+
+if [[ $status_code -eq 200 ]]; then
+  pokemon_data=${response::-3}
+  data=$(echo "${pokemon_data}" | jq -r '"Id=\(.id), name=\(.name), weight=\(.weight), height=\(.height), order=\(.order)"')
+  echo "${data}"
+else
+  echo "Pokemon not found."
+  exit 1
 fi
 
